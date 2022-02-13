f027fbe32c09cf136739dcd6ac35daf3297d5189
aotten <alina.otten@twt.de>
2022-02-13 17:39:55 +0100

    add bar chart for each statistic value

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -8,11 +8,11 @@ export const Score: FC<ScoreProps> = ({ max = 100}): ReactElement => {
   const [value, setValue] = React.useState(0);
 
   React.useEffect(() => {
-    setValue(90);
+    setValue(20);
   }, []);
 
   return (
-    <div className="bg-lightblue mt-8 overflow-hidden flex flex-col justify-center items-center">
+    <div className="bg-lightblue mt-2 overflow-hidden flex flex-col justify-center items-center">
       <span className="text-8xl font-bold text-red">{value}</span>
       <span className="text-2xl font-medium text-green">out of {max}</span>
     </div>
