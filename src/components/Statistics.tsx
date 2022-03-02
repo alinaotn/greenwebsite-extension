39bc77a0177566721d4a498d8c440a032594dc17
aotten <alina.otten@twt.de>
2022-03-02 09:27:32 +0100

    change text size

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -16,7 +16,7 @@ export const Statistics: FC<StatisticsProps> = ({values}): ReactElement => {
     <div className="bg-offwhite mt-8 overflow-hidden flex flex-col justify-center p-2">
       {values && values.map((v) =>
         <div className="mb-4">
-          <span className="text-green">{v.name}</span>
+          <span className="text-green text-base">{v.name}</span>
           <Bar value={v.value}/>
         </div>
       )}
