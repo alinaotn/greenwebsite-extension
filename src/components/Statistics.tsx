4da514a631ef077eba4baf9ed0bcf37ac3f53789
aotten <alina.otten@twt.de>
2022-03-02 11:04:48 +0100

    fetch green hosting api and set green hosting value

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -1,5 +1,6 @@
-import React, {FC, ReactElement} from 'react';
+import React, {FC, ReactElement, useEffect} from 'react';
 import {Bar} from "./Bar";
+import {ClipLoader} from "react-spinners";
 
 type StatisticValues = {
   name: string;
@@ -11,15 +12,30 @@ interface StatisticsProps {
 }
 
 export const Statistics: FC<StatisticsProps> = ({values}): ReactElement => {
+  const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    let timeout: any;
+    if (loading) {
+      timeout = setTimeout(() => setLoading(false), 1000);
+    }
+    return () => clearTimeout(timeout);
+  }, [loading]);
 
   return (
-    <div className="bg-offwhite mt-8 overflow-hidden flex flex-col justify-center p-2">
-      {values && values.map((v) =>
-        <div className="mb-4">
-          <span className="text-green text-base">{v.name}</span>
-          <Bar value={v.value}/>
-        </div>
-      )}
-    </div>
+    <>
+      {loading ?
+        <div className="flex justify-center items-center h-full">
+          <ClipLoader color="#67837E" loading={loading} size={100}/>
+        </div> :
+        <div className="bg-offwhite mt-8 overflow-hidden flex flex-col justify-center p-2">
+          {values && values.map((v, index) =>
+            <div key={index} className="mb-4">
+              <span className="text-green text-base">{v.name}</span>
+              <Bar value={v.value}/>
+            </div>
+          )}
+        </div>}
+    </>
   );
 }
